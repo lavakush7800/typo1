e90783ce7841e32c2d42b40c18bf92aba8f7d8aa
lavakush7800 <lavakushgiri@gmail.com>
2021-10-06 22:16:57 +0530

    slider

--- resources/views/productShow.blade.php
+++ resources/views/productShow.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-  @foreach($results as $item)
-      <div class="{{$item['id']==1?'active':''}}">
-          <img src="<?php echo "/storage/".str_replace('public/','',$item['image']); ?>" alt="Lav">
-      </div>
-      @endforeach
+<div class="container custom-product">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+    @foreach($results as $item)
+        <div class="{{$item['id']==1?'active':''}}">
+            <img src="<?php echo "/storage/".str_replace('public/','',$item['image']); ?>" alt="Lav">
+        </div>
+        @endforeach
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
 @endsection
