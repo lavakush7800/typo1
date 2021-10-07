e46f69709331a97c216df399a97cbc37d27198e1
lavakush7800 <lavakushgiri@gmail.com>
2021-10-07 17:39:33 +0530

    slider

--- resources/views/productShow.blade.php
+++ resources/views/productShow.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container custom-product">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     </ol>
     <div class="carousel-inner">
-    @foreach($results as $item)
-        <div class="{{$item['id']==1?'active':''}}">
-            <img src="<?php echo "/storage/".str_replace('public/','',$item['image']); ?>" alt="Lav">
+        @foreach($results as $item)
+        
+        <div class="carousel-item {{$item == 1 ? 'active' : ''}}">
+           
+            <img src="<?php echo "/storage/".str_replace('public/','',$item['image']); ?>" class="d-block w-100"  alt="...">
         </div>
         @endforeach
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+    <a class="carousel-control-prev" href="#myCarousel" role="button"  data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true">     </span>
+        <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
     </a>
-  </div>
 </div>
 @endsection
