d90255a8a6ea24ae02009a6f0518a2955bb3fa8d
lavakush7800 <lavakushgiri@gmail.com>
2021-10-14 17:53:06 +0530

    modify code

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-// use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\ClientsController;
 
 /*
 |--------------------------------------------------------------------------
